cf3463f4bd37bf3765a167ab80c4a0496b1f19b7
bmurphy1993 <62387896+bmurphy1993@users.noreply.github.com>
2020-11-01 15:35:40 -0500

    t2g

---
+++ transcript-to-gene-import_Group_7.R
@@ -0,0 +1,40 @@
+### LOAD REQUIRED LIBRARIES
+library(biomaRt)
+library(tximport)
+library(rhdf5)
+
+### SET WORKING DIRECTORY ### You will need to edit this and direct it your downloaded kallisto folder
+setwd("C:/Users/bmurp/OneDrive/Desktop/NYU/Classes/Fall_2020/Bioinformatics/03_Crash_Course_in_Gene_Expression/Assignments/bp_outputs")
+
+### IMPORT ENSEMBl ANNOTATIONS FOR HUMAN GENOME & GENERATE TWO COLUMN FILE LINKING TRANSCRIPT AND GENE IDS
+mart <- biomaRt::useMart(biomart = "ensembl", dataset =  "hsapiens_gene_ensembl")
+
+# t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "transcript_version", "ensembl_gene_id", "external_gene_name", "description", "transcript_biotype", "refseq_mrna", "refseq_ncrna"), mart = mart)
+t2g_1 <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "transcript_version", "ensembl_gene_id", "external_gene_name"), mart = mart)
+t2g_2 <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "description", "transcript_biotype", "refseq_mrna", "refseq_ncrna"), mart = mart)
+t2g <- merge(t2g_1, t2g_2, by="ensembl_transcript_id")
+
+t2g$target_id <- paste(t2g$ensembl_transcript_id, t2g$transcript_version, sep=".") # append version number to the transcript ID
+t2g[,c("ensembl_transcript_id","transcript_version")] <- list(NULL) # delete the ensembl transcript ID and transcript version columns
+t2g <- dplyr::rename( t2g, gene_symbol = external_gene_name, full_name = description, biotype = transcript_biotype )
+t2g<-t2g[,c(ncol(t2g),1:(ncol(t2g)-1))]
+
+### GENERATE SECOND OBJECT CONTAINING ONLY PROTEIN CODING GENES
+gb <- getBM(attributes=c("ensembl_gene_id","gene_biotype"), mart=mart)
+gb_coding<-subset(gb, gb$gene_biotype=="protein_coding")
+genes<-gb_coding$ensembl_gene_id
+
+### USE TXIMPORT TO SUMMARIZE TRANSCRIPT COUNTS INTO GENE COUNTS
+## For multiple samples, each named as a folder in the kallisto directory (can be abundance.h5 or abundance.tsv file)
+accessions <- list.dirs(full.names=FALSE)[-1]
+kallisto.dir<-paste0(accessions)
+kallisto.files<-file.path(kallisto.dir,"abundance.tsv") #can also be abundance.tsv
+names(kallisto.files)<- accessions
+tx.kallisto <- tximport(kallisto.files, type = "kallisto", tx2gene = t2g, countsFromAbundance ="no", ignoreAfterBar = TRUE)
+
+### GENERATE TWO COLUMN OUTPUT FORMAT
+counts<-as.data.frame(tx.kallisto$counts[row.names(tx.kallisto$counts) %in% genes, ])
+ids<-rownames(counts)
+
+### ROUND VALUES (DESEQ2 DOES NOT LIKE FRACTIONS), AND WRITE TO OUTPUT FILE
+write.table(round(counts),paste("t2g_output",".txt",sep=""), row.names=ids, quote=F, col.names=T, sep="\t")
